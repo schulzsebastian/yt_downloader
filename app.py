9180e8fa7f82ae2f2b29f380c835f2fc040882e9
schulzsebastian <schulz.siwy@gmail.com>
2016-08-15 18:37:21 +0200

    start proj

---
+++ app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        url = request.values.get('url')
+        if not url:
+            url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        return redirect(url_for('download', url=url))
+
+
+@app.route('/download')
+def download():
+    url = request.args.get('url')
+    return render_template('download.html', url=url)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
