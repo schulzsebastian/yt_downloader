2359617c9259bac9213d98e88c8ff3bc5b771a1a
Sebastian Schulz <schulz.siwy@gmail.com>
2016-08-17 01:11:26 +0200

    script + mp3 file to save

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session, send_file
+import os 
+from script import *
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'xxx'
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,16 +15,13 @@ def index():
         return render_template('index.html')
     else:
         url = request.form.get('url')
-        if not url:
-            url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-        return redirect(url_for('download', url=url))
-
+        session['metadata'] = get_url_metadata(url)
+        return redirect(url_for('download'))
 
 @app.route('/download')
 def download():
-    url = request.args.get('url')
-    return render_template('download.html', url=url)
-
+    filename = download_from_url(session['metadata']['url'])
+    return send_file(app.static_folder + '/' + filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- script.py
+++ script.py
@@ -5,20 +5,30 @@ from __future__ import unicode_literals
 import os
 import youtube_dl
 
-def download_from_url(url):
-    ydl_opts = {
+ydl_opts = {
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         'preferredquality': '192',
         }],
-    }
+    'outtmpl': 'static/%(title)s.%(ext)s'
+}
+
+default_url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+
+def download_from_url(url):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        metadata = ydl.extract_info(url, download=True)
-        filename = metadata['title'] + '-' + metadata['display_id'] + '.mp3'
-    #del
-    os.remove(filename)
+        try:
+            metadata = ydl.extract_info(url, download=True)
+        except:
+            metadata = ydl.extract_info(default_url, download=True)
+        return metadata['title'] + '.mp3'
 
-
-download_from_url('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+def get_url_metadata(url):
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            data = ydl.extract_info(url, download=False)
+        except:
+            data = ydl.extract_info(default_url, download=False)
+        return {'title': data['title'], 'url': data['url']}
