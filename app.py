8bbaf2ce8c05179641c76387eab0375739355782
schulzsebastian <schulz.siwy@gmail.com>
2016-08-17 23:42:13 +0200

    pep8

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, render_template, redirect, url_for, session, send_from_directory
-from werkzeug.utils import secure_filename
-import os 
-from script import *
+from script import youtube_connector
+from flask import Flask, request, render_template, redirect, \
+    url_for, session, send_from_directory
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'xxx'
@@ -19,15 +18,20 @@ def index():
         session['metadata'] = youtube_connector(url, False)
         return redirect(url_for('link'))
 
+
 @app.route('/link')
 def link():
     return render_template('download.html', metadata=session['metadata'])
 
+
 @app.route('/download')
 def download():
     file = youtube_connector(session['metadata']['url'], True)
-    filename = file['title'] + '.mp3'
-    return send_from_directory(app.static_folder, filename, as_attachment=True, attachment_filename="%s.mp3" % filename.encode('utf-8'))
+    filename = file['title'].encode('utf-8') + '.mp3'
+    return send_from_directory(app.static_folder,
+                               filename,
+                               attachment_filename=filename,
+                               as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- script.py
+++ script.py
@@ -2,22 +2,20 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-import os
 import youtube_dl
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-        }],
-    'outtmpl': 'static/%(title)s.%(ext)s'
-}
-
-default_url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 
 def youtube_connector(url, download):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': 'static/%(title)s.%(ext)s'
+    }
+    default_url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         try:
             metadata = ydl.extract_info(url, download=download)
